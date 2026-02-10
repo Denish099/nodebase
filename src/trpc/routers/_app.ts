f538b46a7d2dc1af4b66f7bf783927304fddceb1
Denish099 <gyldenish@gmail.com>
2026-02-10 19:31:28 +0530

    cleaned _app.ts and added protectedProcedure of workflow

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,27 +1,11 @@
-import { createTRPCRouter, protectedProcedure } from "../init";
-import { prisma } from "@/lib/db";
-import { inngest } from "@/inngest/client";
+import { workflowsRouter } from "@/features/workflows/server/routers";
+import { createTRPCRouter  } from "../init";
+
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
-    await inngest.send({ name: "execute.ai" });
 
-    return { success: true, message: "job queued" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    console.log({ userid: ctx.auth.user.id });
-    console.log(Object.keys(prisma));
-
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "densih",
-      },
-    });
-  }),
+  workflows: workflowsRouter,
+  
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
